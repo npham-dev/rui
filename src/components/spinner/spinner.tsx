cae5a92643d66e832fcc710514394617a1f19ce5
faymn <256236277+faymndev@users.noreply.github.com>
2026-04-07 10:40:05 -0700

    better spinner

--- src/components/spinner/spinner.tsx
+++ src/components/spinner/spinner.tsx
@@ -10,7 +10,7 @@ type SpinnerProps = View.Props;
 function Spinner({ className, ...props }: SpinnerProps) {
   return (
     <View className={clsx(styles["spinner"], className)} {...props}>
-      <Icon name="loader-2-line" />
+      <Icon name="loader-3-line" />
     </View>
   );
 }
